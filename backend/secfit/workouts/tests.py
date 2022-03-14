0953140a50ad13ecf88c2fda86e895797ae5356b
Brandon Yeow <brandonywl@gmail.com>
2022-03-14 06:19:03 +0100

    Added visualization of coverage for CI/CD

--- backend/secfit/workouts/tests.py
+++ backend/secfit/workouts/tests.py
@@ -4,3 +4,17 @@ Tests for the workouts application.
 from django.test import TestCase
 
 # Create your tests here.
+
+class CurrentUserPermissionTest(TestCase):
+    def setUp(self):
+        pass
+
+
+class AthletePermissionTest(TestCase):
+    def setUp(self):
+        pass
+
+
+class CoachPermissionTest(TestCase):
+    def setUp(self):
+        pass
